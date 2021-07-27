7eacfbe0299f623cb30783e2654a1512d51ff49f
Óscar Múnera <43389250+oscarmunera@users.noreply.github.com>
2021-07-27 15:45:39 -0500

    Taller finalizado

--- figuras.js
+++ figuras.js
@@ -1,8 +1,49 @@
-const ladoCuadrado =5;
-console.log("Los lados del cuadrado miden: " +ladoCuadrado)
+//Código del cuadrado
 
-const perimetroCuadrado =ladoCuadrado * 4;
-console.log("El perímetro del cuadrado es: " + perimetroCuadrado);
+function perimetroCuadrado(lado) {
+    return lado*4;
+};
+function areaCuadrado(lado){
+    return lado * lado;
+};
+//Código del triángulo
+function perimetroTriangulo (lado1,lado2,base){
+    return lado1+lado2+base;
+}
+function areaTriangulo(base,altura){
+    return (base*altura)/2;
+}
+//Código del círculo
 
-const areaCuadrado =ladoCuadrado *ladoCuadrado;
-console.log("El área del cuadrado es: " + areaCuadrado);
+function diametroCirculo(radio) {
+    return radio * 2;    
+}
+//pi
+const PI = Math.PI;
+//circunferencia
+function perimetroCirculo(radio){
+  const diametro =diametroCirculo(radio);
+    return diametro*PI;
+}
+//área del círculo
+function areaCirculo (radio){
+    return (radio*radio)*PI;
+}
+
+
+function calcularPerimetroCuadrado(){
+    const input =document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+
+}
+
+function calcularAreaCuadrado(){
+    const input =document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
