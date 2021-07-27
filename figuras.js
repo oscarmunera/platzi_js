2080b3d748689a0a30ec9124eead62e252b5d3ce
Óscar Múnera <43389250+oscarmunera@users.noreply.github.com>
2021-07-27 12:47:41 -0500

    Se crearon los archivos .html y js del taller

---
+++ figuras.js
@@ -0,0 +1,8 @@
+const ladoCuadrado =5;
+console.log("Los lados del cuadrado miden: " +ladoCuadrado)
+
+const perimetroCuadrado =ladoCuadrado * 4;
+console.log("El perímetro del cuadrado es: " + perimetroCuadrado);
+
+const areaCuadrado =ladoCuadrado *ladoCuadrado;
+console.log("El área del cuadrado es: " + areaCuadrado);
