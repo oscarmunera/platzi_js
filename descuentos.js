b761b64dad0eacdcd7b3282cd02d7b85bb3ae354
Óscar Múnera <43389250+oscarmunera@users.noreply.github.com>
2021-07-29 11:11:02 -0500

    Adición de taller 3-promedio

--- descuentos.js
+++ descuentos.js
@@ -5,9 +5,36 @@ return precioConDescuento;
 }
 
 
+const couponDiscount =[
+
+ { name:  "AA001",
+   discount: 30,
+},
+{ name: "AB001",
+  discount : 35,
+},
+{name :"AC001",
+discount : 40},
+]
+
+
+
+
+
 function onClickButtonPriceDiscount() {
 const inputPrice = document.getElementById("InputPrice");
 const priceValue = inputPrice.value;
+
+const inputCoupon = document.getElementById("InputCoupon");
+const valueCoupon = inputCoupon.value;
+
+const isCouponValueValid = couponDiscount.find( descuento => descuento.name === valueCoupon );
+if (!isCouponValueValid) {
+    alert("El cupon:" + valueCoupon + " no es válido");
+} else {
+    discountValue = isCouponValueValid.discount;
+}
+
 
 const inputDiscount = document.getElementById("InputDiscount");
 const discountValue = inputDiscount.value;
@@ -18,3 +45,4 @@ const resultP = document.getElementById("ResultP");
 resultP.innerText = "El precio con descuento es: $" + precioConDescuento;
 
 }
+
